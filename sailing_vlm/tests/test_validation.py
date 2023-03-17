19f0eb100b54c119e950c0527dd06bf8ceb37a37
Zuzanna Wieczorek <zuzanna30.wieczorek@gmail.com>
2023-03-17 02:41:09 +0100

    add sweep validation, correct flat plate

--- sailing_vlm/tests/test_validation.py
+++ sailing_vlm/tests/test_validation.py
@@ -1,42 +1,19 @@
 import numpy as np
+import math
 from unittest import TestCase
 
 import sailing_vlm.runner.aircraft as ac
 from sailing_vlm.rotations.csys_transformations import CSYS_transformations
 from sailing_vlm.runner.sail import Wind, Sail
 from sailing_vlm.solver.vlm import Vlm
+from sailing_vlm.results.inviscid_flow import InviscidFlowResults
 from sailing_vlm.solver.coefs import get_vlm_Cxyz
+from sailing_vlm.rotations.geometry_calc import rotation_matrix
 
 
 class TestValidation(TestCase):
-    
-#     >>> 
-# >>> 
-# >>> )
-# >>> a.get_Cxyz()
-# (0.023472780216173314, 0.8546846987984326, 0.0, array([0.14377078, 5.23494378, 0.        ]), array([1., 0., 0.]), 10.0, 6.125)
-# ```
-# SAIL
-# 0.023472780216173342 0.8546846987984335 6.996235308182204e-34 [1.43770779e-01 5.23494378e+00 4.28519413e-33] [1. 0. 0.] 10.0 6.125
-#     
-    def test_flat_plate(self):
-        # AIRCRAFT
-        chord_length = 1.0
-        half_span_length = 5.0
-        AoA = 10.0
-        n_spanwise = 32
-        n_chordwise = 8
-        V = np.array([1.0, 0., 0.])
-        rho = 1.225
-        gamma_orientation = -1.0
-        
-        a = ac.Aircraft(chord_length, half_span_length, AoA, n_spanwise, n_chordwise, V, rho, gamma_orientation)
-        aircraft_Cxyz = a.Cxyz           
-        # SAIL
-    
-        # SOLVER SETTINGS
-        n_spanwise = 16  # No of control points (above the water) per sail, recommended: 50
-        n_chordwise = 8 # No of control points (above the water) per sail, recommended: 50
+    def calculate_main_sail(self, rho: float, tws_ref: float, chord_length: float, half_wing_span:  float, AoA_deg: float, sweep_angle_deg : float, n_spanwise: int, n_chordwise: int ):
+
         interpolation_type = "linear"  # either "spline" or "linear"
         LLT_twist = "real_twist"  # defines how the Lifting Line discretize the sail twist.
         # It can be "sheeting_angle_const" or "average_const" or "real_twist"
@@ -45,31 +22,29 @@ class TestValidation(TestCase):
         leeway_deg = 0.    # [deg]
         heel_deg = 0.     # [deg]
         SOG_yacht = 0.   # [m/s] yacht speed - speed over ground (leeway is a separate variable)
-        tws_ref = 1.     # [m/s] true wind speed
-        alpha_true_wind_deg = 0.   # [deg] true wind angle (with reference to course over ground) => Course Wind Angle to the boat track = true wind angle to centerline + Leeway
+        alpha_true_wind_deg = AoA_deg   # [deg] true wind angle (with reference to course over ground) => Course Wind Angle to the boat track = true wind angle to centerline + Leeway
         reference_water_level_for_wind_profile = 0.  # [m] this is an attempt to mimick the deck effect
         wind_exp_coeff = 0.  # [-] coefficient to determine the exponential wind profile
         wind_reference_measurment_height = 10.  # [m] reference height for exponential wind profile
-        rho = 1.225  # air density [kg/m3]
         wind_profile = 'flat' # allowed: 'exponential' or 'flat' or 'logarithmic'
         roughness = 0.05 # for logarithmic profile only
 
         # GEOMETRY OF THE RIG
-        main_sail_luff = 5.0 #10. # 12.4  # [m]
+        main_sail_luff = half_wing_span / np.cos(np.deg2rad(sweep_angle_deg)) # [m]
         jib_luff = 10.0  # [m]
         foretriangle_height = 11.50  # [m]
         foretriangle_base = 3.90  # [m]
         sheer_above_waterline = 0.  # [m]
         boom_above_sheer = 0. #1.3  # [m]
-        rake_deg = 90.  # rake angle [deg]
+        rake_deg = 90. + sweep_angle_deg # rake angle [deg]
         mast_LOA = 0.0  # [m]
 
         # INPUT - GEOMETRY OF THE SAIL
         sails_def = 'main' # definition of sail set, possible: 'jib' or 'main' or 'jib_and_main'
         main_sail_girths = np.array([0.00, 1./8, 1./4, 1./2, 3./4, 7./8, 1.00])
-        main_sail_chords = np.array([1.00]* len(main_sail_girths)) # np.array([4.00, 3.82, 3.64, 3.20, 2.64, 2.32, 2.00])
-        main_sail_centerline_twist_deg = -10+ 0* main_sail_girths # 10 + 12. * main_sail_girths  #
-
+        main_sail_chords = np.array([chord_length]* len(main_sail_girths)) # np.array([4.00, 3.82, 3.64, 3.20, 2.64, 2.32, 2.00])
+        main_sail_centerline_twist_deg = 0* main_sail_girths
+        
         # First digit describing maximum camber as percentage of the chord.
         # Second digit describing the distance of maximum camber from the airfoil leading edge in tenths of the chord.
         jib_sail_camber= 0*np.array([0.01, 0.01, 0.01, 0.01, 0.01])
@@ -79,14 +54,10 @@ class TestValidation(TestCase):
 
         jib_girths = np.array([0.00, 1./4, 1./2, 3./4, 1.00])
         jib_chords = 0*np.array([3.80, 2.98, 2.15, 1.33, 0.5]) - 0*0.4
-        jib_centerline_twist_deg =  0*(10+5)  + 0*15. * jib_girths # (10+5)  + 15. * jib_girths # 
+        jib_centerline_twist_deg =  0*jib_girths 
 
         reference_level_for_moments = np.array([0, 0, 0])  # [yaw_reference, sway_reference, heeling_reference]
-
-        # GEOMETRY OF THE KEEL
-        center_of_lateral_resistance_upright = np.array([0, 0, -1.0])  # [m] the coordinates for a yacht standing in upright position
-
-        
+    
         csys_transformations = CSYS_transformations(
         heel_deg, leeway_deg,
         v_from_original_xyz_2_reference_csys_xyz=reference_level_for_moments)
@@ -103,13 +74,88 @@ class TestValidation(TestCase):
         sail_set = s.sail_set
         myvlm = Vlm(sail_set.panels, n_chordwise, n_spanwise, rho, w_profile, sail_set.trailing_edge_info, sail_set.leading_edge_info)
         
+        inviscid_flow_results = InviscidFlowResults(sail_set, csys_transformations, myvlm)
+      
+        A = rotation_matrix([0, 0, 1], np.deg2rad(-AoA_deg))
+        sail_mirror_multiplier = 2 #???
+        F_xyz_total = sail_mirror_multiplier*inviscid_flow_results.F_xyz_total
+        F = np.dot(A, F_xyz_total)  # By definition, the lift force is perpendicular to V_inf
+         
+        S = 2 * half_wing_span * chord_length
+        q = 0.5 * rho * (np.linalg.norm(tws_ref) ** 2) * S
+        sail_Cxyz = F / q
+    
+        return sail_Cxyz, F, myvlm
+
+    def test_sweep_wing(self):
+        # page 365, Example 7.2 Aerodynamics for engineers, John J.Bertin
+        # https://airloads.net/Downloads/Textbooks/Aerodynamics-for-engineers-%20John%20J.Bertin.pdf
         
-        S = 2*main_sail_luff * main_sail_chords[0]
-        sail_Cxyz = get_vlm_Cxyz(myvlm.force, np.array(w_profile.get_true_wind_speed_at_h(1.0)), rho, S)
+        chord_length = 0.2             
+        half_wing_span = 0.5 
+        AoA_deg = 4.
+        sweep_angle_deg = 45.
+        tws_ref = 1.0
+        rho = 1.0
+        # SOLVER SETTINGS
+        n_spanwise = 4  # No of control points (above the water) per sail, recommended: 50
+        n_chordwise = 1 # No of control points (above the water) per sail, recommended: 50
         
-        # print(f'{Cxyz[0]} vs {sail_Cxyz[0]}')
-        # print(f'{Cxyz[1]} vs {sail_Cxyz[1]}')
-        # print(f'{Cxyz[2]} vs {sail_Cxyz[2]}')
+        sail_Cxyz, F, myvlm = self.calculate_main_sail(rho, tws_ref, chord_length, half_wing_span, AoA_deg, sweep_angle_deg, n_spanwise, n_chordwise)
+        
+        a_VLM = sail_Cxyz[1] / np.deg2rad(AoA_deg)
+        np.testing.assert_almost_equal(myvlm.rings[3,1], np.array([0.425, 0., 0.375]), decimal=3)
+        np.testing.assert_almost_equal(myvlm.rings[3, 2], np.array([0.550, 0., 0.500]), decimal=3)
+        np.testing.assert_almost_equal(myvlm.ctr_p[3], np.array([0.5875, 0., 0.4375]), decimal=4)
+        np.testing.assert_almost_equal(myvlm.gamma_magnitude[0:4], np.array([0.0239377, 0.02521229, 0.02511784, 0.02187719]), decimal=4)
+        np.testing.assert_almost_equal(F, np.array([0.00031786, 0.02402581, 0.00135887]), decimal=5)
+        np.testing.assert_almost_equal(sail_Cxyz[1],0.240258, decimal=4)
+        np.testing.assert_almost_equal(a_VLM, 3.441443, decimal=2)
+        
+        
+        gamma_ref = 4*np.pi*2*half_wing_span*tws_ref*np.deg2rad(AoA_deg)*np.array([0.0273, 0.0287, 0.0286, 0.0250]) #get_gamma_reference(2*half_wing_span, tws_ref, AoA_deg) # Eq 7.48
+        dy = 2*half_wing_span/(2*len(gamma_ref))
+        L_ref = 2*rho*tws_ref*tws_ref*np.sum(gamma_ref*dy)    #eq 7.50b
+        
+        S = 2 * half_wing_span * chord_length
+        q = 0.5 * rho * (np.linalg.norm(tws_ref) ** 2) * S
+        
+        CL_ref = L_ref / q
+        a_ref = CL_ref / np.deg2rad(AoA_deg)
+
+        L_ref2 = rho * tws_ref * tws_ref * 4 * half_wing_span * half_wing_span * np.pi * np.deg2rad(AoA_deg) * 0.1096
+        CL_ref2 = 1.096*np.pi*np.deg2rad(AoA_deg)
+
+        np.testing.assert_almost_equal(L_ref, L_ref2, decimal=6)
+        np.testing.assert_almost_equal(L_ref, F[1], decimal=4)
+        np.testing.assert_almost_equal(CL_ref, CL_ref2, decimal=6)
+        np.testing.assert_almost_equal(CL_ref, 0.240379, decimal=6)
+        np.testing.assert_almost_equal(a_ref, 3.443185, decimal=6)
+    
+    def test_flat_plate(self):
+        # page 365, Example 7.2 Aerodynamics for engineers, John J.Bertin
+        # https://airloads.net/Downloads/Textbooks/Aerodynamics-for-engineers-%20John%20J.Bertin.pdf
+        
+        chord_length = 1.0             
+        half_wing_span = 5.0 
+        AoA_deg = 10.
+        sweep_angle_deg = 0.
+        tws_ref = 1.0
+        V = np.array([tws_ref, 0., 0.])
+        rho = 1.225
+        gamma_orientation = -1.0
+        n_spanwise = 32  # No of control points (above the water) per sail, recommended: 50
+        n_chordwise = 8 # No of control points (above the water) per sail, recommended: 50
+        
+        # AIRCRAFT
+        a = ac.Aircraft(chord_length, half_wing_span, AoA_deg, n_spanwise, n_chordwise, V, rho, gamma_orientation)
+        aircraft_Cxyz = a.Cxyz 
+
+        # n_spanwise / 2 because SAIL has underwater part also
+        sail_Cxyz, F, myvlm = self.calculate_main_sail(rho, tws_ref, chord_length, half_wing_span, AoA_deg, sweep_angle_deg, int(n_spanwise / 2), n_chordwise)
+        
         np.testing.assert_almost_equal(aircraft_Cxyz[0], sail_Cxyz[0])
         np.testing.assert_almost_equal(aircraft_Cxyz[1], sail_Cxyz[1])
         np.testing.assert_almost_equal(aircraft_Cxyz[2], sail_Cxyz[2])
+        
+        
